ab427b6e41ebf47fca77cb9bd3fcd146fe319fc9
Jeon sumin <sumin1236@naver.com>
2022-06-19 20:41:17 +0900

    heroku upload

--- polls/views.py
+++ polls/views.py
@@ -15,7 +15,6 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
 
